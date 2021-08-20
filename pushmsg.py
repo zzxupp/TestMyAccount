db528174b534f6fdd78ae7753b229d7211f852e1
zzxupp <75357370+zzxupp@users.noreply.github.com>
2021-08-20 21:19:58 +0800

    Update pushmsg.py

--- pushmsg.py
+++ pushmsg.py
@@ -27,6 +27,6 @@ def qmsg(qmsg_key, qq, message):
         print('Qmsg酱 推送成功')
 
 
-qmsg(QMSG_KEY, QQ, '@face=181@ 沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + '@face=181@ GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG + '@face=181@ Euserv - 签到提醒:\n' + euserv.EUSERV_MSG)
-qw360(QW360_TOKEN, '沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + 'GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG  + 'Euserv - 签到提醒:\n' + euserv.EUSERV_MSG)   
+qmsg(QMSG_KEY, QQ, '@face=181@ 沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + '@face=181@ GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG + '\n\n' + '@face=181@ Euserv - 签到提醒:\n' + euserv.EUSERV_MSG)
+qw360(QW360_TOKEN, '沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + 'GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG  + '\n\n' + 'Euserv - 签到提醒:\n' + euserv.EUSERV_MSG)   
 print('沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + 'GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG)
