34a5178daeba82d4c79b49a0c5f3e3c9fc7f85a0
zzxupp <75357370+zzxupp@users.noreply.github.com>
2021-08-20 19:19:07 +0800

    Create pushmsg.py

---
+++ pushmsg.py
@@ -0,0 +1,33 @@
+import json,os,re,requests
+import womail
+import glados
+
+QW360_TOKEN = os.getenv('QW360_TOKEN')
+QQ = os.environ["QQ"]
+QMSG_KEY = os.environ["QMSG_KEY"]
+
+def qw360(QW360_TOKEN, message):
+    response = requests.get('https://push.bot.qw360.cn/send/' + QW360_TOKEN + '?msg=' + message).json()
+    if (response["status"]) != 1:
+        print('qw360 推送失败')
+    else:
+        print('qw360 推送成功') 
+
+def qmsg(qmsg_key, qq, message): 
+    urls='https://qmsg.zendee.cn/send/' + qmsg_key   #私聊消息推送接口
+    data = {
+        "qq": qq,
+        "msg": message
+    }
+    response = requests.post(urls,data=data).json()
+    if int(response["code"] / 100) != 0:
+        print('Qmsg酱 推送失败')
+    else:
+        print('Qmsg酱 推送成功')
+
+def main_handler(event, context):
+  return start()
+
+if __name__ == '__main__':
+qmsg(QMSG_KEY, QQ, '@face=181@ 沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + '@face=181@ GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG)
+qw360(QW360_TOKEN, '沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + 'GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG)
