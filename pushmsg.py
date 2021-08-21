9f1a907ed3174095837798a0fd471da8eee19482
zzxupp <75357370+zzxupp@users.noreply.github.com>
2021-08-21 13:37:47 +0800

    Update pushmsg.py

--- pushmsg.py
+++ pushmsg.py
@@ -29,7 +29,7 @@ def qmsg(qmsg_key, qq, message):
 
 msg = 'bilibili.BiliBiliCheckIn(bilibili_cookie_list=_bilibili_cookie_list).main()'
 
-qmsg(QMSG_KEY, QQ, BILIBILI_MSG)
+qmsg(QMSG_KEY, QQ, bilibili.BILIBILI_MSG)
 qw360(QW360_TOKEN, msg)
-qw360(QW360_TOKEN, BILIBILI_MSG)
+qw360(QW360_TOKEN, bilibili.BILIBILI_MSG)
 #print('沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + 'GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG)
