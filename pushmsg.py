670dcdc8b4a4b2b0c659f80504ab8d6f7fef1dd7
zzxupp <75357370+zzxupp@users.noreply.github.com>
2021-08-21 16:45:21 +0800

    Update pushmsg.py

--- pushmsg.py
+++ pushmsg.py
@@ -1,7 +1,7 @@
 import json,os,re,requests
-#import womail
-#import glados
-#import euserv
+from womail import WOMAIL_MSG
+from glados import GLADOS_MSG
+from euserv import EUSERV_MSG
 from bilibili import BILIBILI_MSG
 
 QW360_TOKEN = os.getenv('QW360_TOKEN')
@@ -29,7 +29,7 @@ def qmsg(qmsg_key, qq, message):
 
 msg = 'bilibili.BiliBiliCheckIn(bilibili_cookie_list=_bilibili_cookie_list).main()'
 
-qmsg(QMSG_KEY, QQ, BILIBILI_MSG)
-qw360(QW360_TOKEN, BILIBILI_MSG)
-print(BILIBILI_MSG)
+qmsg(QMSG_KEY, QQ, WOMAIL_MSG + '\n\n' + GLADOS_MSG + '\n\n' + BILIBILI_MSG + '\n\n' + EUSERV_MSG)
+qw360(QW360_TOKEN, WOMAIL_MSG + '\n\n' + GLADOS_MSG + '\n\n' + BILIBILI_MSG + '\n\n' + EUSERV_MSG)
+print(WOMAIL_MSG + '\n\n' + GLADOS_MSG + '\n\n' + BILIBILI_MSG + '\n\n' + EUSERV_MSG)
 #print('沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + 'GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG)
