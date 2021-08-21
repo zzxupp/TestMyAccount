166407c5006f5ddb25c0b579885b1534b9d632c2
zzxupp <75357370+zzxupp@users.noreply.github.com>
2021-08-21 17:55:10 +0800

    Update pushmsg.py

--- pushmsg.py
+++ pushmsg.py
@@ -1,8 +1,8 @@
 import json,os,re,requests
-#from womail import WOMAIL_MSG
-#from glados import GLADOS_MSG
+from womail import WOMAIL_MSG
+from glados import GLADOS_MSG
 from euserv import EUSERV_MSG
-#from bilibili import BILIBILI_MSG
+from bilibili import BILIBILI_MSG
 
 QW360_TOKEN = os.getenv('QW360_TOKEN')
 QQ = os.environ["QQ"]
@@ -27,10 +27,10 @@ def qmsg(qmsg_key, qq, message):
     else:
         print('Qmsg酱 推送成功')
 
-#qmsg(QMSG_KEY, QQ, WOMAIL_MSG + '\n\n' + GLADOS_MSG + '\n\n' + BILIBILI_MSG + '\n\n' + EUSERV_MSG)
-#qw360(QW360_TOKEN, WOMAIL_MSG + '\n\n' + GLADOS_MSG + '\n\n' + BILIBILI_MSG + '\n\n' + EUSERV_MSG)
+qmsg(QMSG_KEY, QQ, WOMAIL_MSG + '\n\n' + GLADOS_MSG + '\n\n' + BILIBILI_MSG + '\n\n' + EUSERV_MSG)
+qw360(QW360_TOKEN, WOMAIL_MSG + '\n\n' + GLADOS_MSG + '\n\n' + BILIBILI_MSG + '\n\n' + EUSERV_MSG)
 #print(WOMAIL_MSG + '\n')
 #print(GLADOS_MSG + '\n')
 #print(BILIBILI_MSG + '\n')
-print(EUSERV_MSG)
+#print(EUSERV_MSG)
 #print('沃邮箱 - 签到提醒:\n' + womail.WOMAIL_MSG + '\n\n' + 'GLaDOS - 签到提醒:\n' + glados.GLADOS_MSG)
