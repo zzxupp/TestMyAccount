f985f5dfe45134a4ee22e91018375ab7d96f5de4
zzxupp <75357370+zzxupp@users.noreply.github.com>
2021-08-27 14:29:06 +0800

    Update weibo.py

--- weibo.py
+++ weibo.py
@@ -125,14 +125,14 @@ def chaohua_checkin(Cookie, item):
 def start():
     #Cookie1 = 'SUB=' + os.environ['SUBM']
     #Cookie2 = 'SUB=' + os.environ['SUBP']
-    Cookie2 = 'SUB=' + os.getenv('SUBP')
+    Cookie1 = 'SUB=' + os.getenv('SUBP')
     # 获取超话列表
-    chaohua_list = get_chaohua_List(Cookie2)
+    chaohua_list = get_chaohua_List(Cookie1)
     print(chaohua_list)
     msg_list = []
     for item in chaohua_list:
         #msg = chaohua_checkin(Cookie1, item)
-        msg = chaohua_checkin(Cookie2, item)
+        msg = chaohua_checkin(Cookie1, item)
         msg_list.append(msg)
         time.sleep(15)
     if push_type == '1':
