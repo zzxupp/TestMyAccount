f31ef5594a50c9fbae5cced7b99c0fbb6d18b661
Zhen Xupin <justdo@email.cn>
2021-09-17 00:01:06 +0800

    Update jegotrip.py

--- jegotrip.py
+++ jegotrip.py
@@ -69,7 +69,7 @@ def getCoins():
         return 'zzzz'
 
 def readcredits(token, sign):
-    resp = requests.get(f'https://app1.jegotrip.com.cn/api/duiba/v1/mall/logonFree?token={token}&url=http://www.duiba.com.cn/autoLogin/autologin&timestamp=1631805453942&sign={sign}')
+    resp = requests.get(f'https://app1.jegotrip.com.cn/api/duiba/v1/mall/logonFree?token=6d8b7e4204764e6cb953d8a859926007&url=http://www.duiba.com.cn/autoLogin/autologin&timestamp=1631805453942&sign=6b59c1e812658c41f3f38099850ecf2346fdae73')
     data = resp.json()
     pprint.pprint(data)
     #_logonFreeUrl = data['body']['logonFreeUrl']
